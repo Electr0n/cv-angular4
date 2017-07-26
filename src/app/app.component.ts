4d1d5a9915b72a931c02cfe61b76d0cd02d7adb4
Electr0n <skorodumov.eugene@gmail.com>
2017-07-26 17:44:14 +0300

    angular 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+export class Lang{
+  id: number;
+  name: string;
+  lvl: number;
+}
+
+const LANGS: Lang[] = [
+  { id: 1, name: 'Ruby', lvl: 6 },
+  { id: 2, name: 'Java', lvl: 5 },
+  { id: 3, name: 'HTML', lvl: 8},
+  { id: 4, name: 'CSS', lvl: 8},
+  { id: 5, name: 'JavaScript', lvl: 4},
+  { id: 6, name: 'Angular 4', lvl: 4},
+  { id: 7, name: 'English', lvl: 6}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'CV';
+  name = 'Skorodumov Eugene';
+  langs = LANGS;
+  selectedLang: Lang;
+
+  onSelect(lang: Lang): void {
+    this.selectedLang = lang;
+  }
 }
+
